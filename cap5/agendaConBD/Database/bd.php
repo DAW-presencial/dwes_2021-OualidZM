dd262c4ccf7ff28d92636ab292626f19a4f2443b
OualidZM <ozaaj@cifpfbmoll.eu>
2021-12-11 00:04:09 +0100

    docs: :sparkles: agenda

---
+++ cap5/agendaConBD/Database/bd.php
@@ -0,0 +1,22 @@
+<?php
+class Databasee{
+    
+    private $host = "localhost";
+    private $user  = "root";
+    private $dbpass = "mysqlroot";
+    public $conn;
+    
+    public function connection(){
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host" . $this->host, $this->user,$this->dbpass);
+
+        }catch(PDOException $error) {
+            echo "Can't connect: " . $error->getMessage();
+            
+        }
+        return  $this->conn;
+    }
+}
+?>
